f983ceb7de16ea30f7b34a007986166a0333fb8e
Zhang Guo <guo.zhang@thoughtworks.com>
2022-08-02 15:36:37 +0800

    add dispatchers di

--- app/src/main/java/com/thoughtworks/android/ark/ui/home/HomeDataSource.kt
+++ app/src/main/java/com/thoughtworks/android/ark/ui/home/HomeDataSource.kt
@@ -1,11 +1,16 @@
 package com.thoughtworks.android.ark.ui.home
 
+import com.thoughtworks.android.ark.di.IoDispatcher
+import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.delay
+import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
-class HomeDataSource @Inject constructor() {
-    suspend fun getData(): String {
+class HomeDataSource @Inject constructor(
+    @IoDispatcher private val ioDispatcher: CoroutineDispatcher
+) {
+    suspend fun getData() = withContext(ioDispatcher) {
         delay(1000)
-        return "Test data"
+        "Test data"
     }
 }
