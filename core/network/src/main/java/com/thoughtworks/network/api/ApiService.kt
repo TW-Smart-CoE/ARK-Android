9d4101005ec54ea5155f34f990e9beee58f6629f
ComeAndGo <yaan.zhu@thoughtworks.com>
2022-08-03 11:29:42 +0800

    [network]modify some issue of retrofit client

--- core/network/src/main/java/com/thoughtworks/network/api/ApiService.kt
+++ core/network/src/main/java/com/thoughtworks/network/api/ApiService.kt
@@ -1,4 +1,8 @@
 package com.thoughtworks.network.api
 
+import retrofit2.http.GET
+
 interface ApiService {
+    @GET("/list/data")
+    suspend fun getAllData()
 }
--- core/network/src/main/java/com/thoughtworks/network/client/RetrofitClient.kt
+++ core/network/src/main/java/com/thoughtworks/network/client/RetrofitClient.kt
@@ -1,5 +1,56 @@
 package com.thoughtworks.network.client
 
-class RetrofitClient {
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
 
+class RetrofitClient {
+    private lateinit var retrofit:Retrofit
+    private lateinit var okHttpClient: OkHttpClient
+    init {
+        initRetrofit(
+            initOkHttpClient = {
+                addLoggingInterceptor(it,true)
+            }
+        )
+    }
+
+    private fun initRetrofit(
+        initOkHttpClient: (OkHttpClient.Builder)-> Unit = {},
+        initRetrofit: (Retrofit.Builder) -> Unit = {}
+    ) {
+        okHttpClient = OkHttpClient.Builder().apply {
+            callTimeout(TIME_OUT, TimeUnit.MICROSECONDS)
+            connectTimeout(TIME_OUT, TimeUnit.MICROSECONDS)
+            readTimeout(TIME_OUT, TimeUnit.MICROSECONDS)
+            writeTimeout(TIME_OUT, TimeUnit.MICROSECONDS)
+            initOkHttpClient(this)
+        }.build()
+
+        retrofit = Retrofit.Builder().apply {
+            baseUrl(HOST_URL)
+            addConverterFactory(GsonConverterFactory.create())
+            client(okHttpClient)
+            initRetrofit(this)
+        }.build()
+    }
+
+    private fun addLoggingInterceptor(builder:OkHttpClient.Builder, isDebug:Boolean = true) {
+        if(isDebug) {
+            builder.addInterceptor(
+                HttpLoggingInterceptor().apply {
+                    level = HttpLoggingInterceptor.Level.BODY
+                }
+            )
+        }
+    }
+
+    fun <T> createService(clazz:Class<T>):T = retrofit.create(clazz)
+
+    companion object {
+        const val HOST_URL = "www.example.com"
+        const val TIME_OUT = 5000L
+    }
 }
