d9efa52ff32dbf492690bc0eec309d23bc65d1f3
Jingsong Zeng <jingsong.zeng@thoughtworks.com>
2022-08-03 12:04:37 +0800

    Add the special color test for system color system.

--- ui-sample/src/main/java/com/thoughtworks/android/ark/uisample/colorsystem/XmlColorSystemActivity.kt
+++ ui-sample/src/main/java/com/thoughtworks/android/ark/uisample/colorsystem/XmlColorSystemActivity.kt
@@ -7,6 +7,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.ComposeView
 import androidx.compose.ui.platform.LocalContext
+import com.thoughtworks.android.ark.ui.themes.colors.XmlColors
 import com.thoughtworks.android.ark.ui.themes.colors.obtainThemeAttrColorToColor
 import com.thoughtworks.android.ark.uisample.R
 
@@ -15,14 +16,19 @@ class XmlColorSystemActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.xml_color_system)
 
-        val composeColor01 = findViewById<ComposeView>(R.id.compose_color_01)
-        composeColor01.setContent {
-            ComposeColorTestContent()
+        val themeColorTestCompose = findViewById<ComposeView>(R.id.compose_color_01)
+        themeColorTestCompose.setContent {
+            ThemeColorTestContent()
+        }
+
+        val specialColorTestCompose = findViewById<ComposeView>(R.id.compose_color_02)
+        specialColorTestCompose.setContent {
+            SpecialColorTestContent()
         }
     }
 
     @Composable
-    private fun ComposeColorTestContent() {
+    private fun ThemeColorTestContent() {
         val colorInt = obtainThemeAttrColorToColor(
             context = LocalContext.current,
             attrResId = com.google.android.material.R.attr.colorPrimary
@@ -30,6 +36,14 @@ class XmlColorSystemActivity : AppCompatActivity() {
         Text(
             text = LocalContext.current.getString(R.string.theme_color_test_for_compose),
             color = Color(colorInt)
+        )
+    }
+
+    @Composable
+    private fun SpecialColorTestContent() {
+        Text(
+            text = LocalContext.current.getString(R.string.special_color_test_for_compose),
+            color = XmlColors.ButtonBackground.colorValue()
         )
     }
 }
