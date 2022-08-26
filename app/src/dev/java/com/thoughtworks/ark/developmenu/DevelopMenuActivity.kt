30bc2fc7cda5f513219495dd5f8c9c1b26b2e863
Zhang Shuai <shuaiz@thoughtworks.com>
2022-08-26 15:36:56 +0800

    update DevelopMenu theme to apptheme

--- app/src/dev/java/com/thoughtworks/ark/developmenu/DevelopMenuActivity.kt
+++ app/src/dev/java/com/thoughtworks/ark/developmenu/DevelopMenuActivity.kt
@@ -3,12 +3,13 @@ package com.thoughtworks.ark.developmenu
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.toArgb
@@ -17,7 +18,10 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.core.view.WindowCompat
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
 import com.thoughtworks.ark.R
+import com.thoughtworks.ark.ui.themes.APPTheme
+import com.thoughtworks.ark.ui.themes.colors.LocalAPPThemeColors
 
 class DevelopMenuActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -25,7 +29,14 @@ class DevelopMenuActivity : AppCompatActivity() {
         setupWindow()
 
         setContent {
-            MaterialTheme {
+            APPTheme {
+                val systemUiController = rememberSystemUiController()
+                val statusBarColor = LocalAPPThemeColors.current.statusBarColor
+
+                LaunchedEffect(statusBarColor) {
+                    systemUiController.setStatusBarColor(statusBarColor)
+                    systemUiController.setNavigationBarColor(statusBarColor)
+                }
                 DevelopMenuScreen()
             }
         }
@@ -42,13 +53,15 @@ class DevelopMenuActivity : AppCompatActivity() {
     @Composable
     fun DevelopMenuScreen() {
         Column(
-            modifier = Modifier.fillMaxSize()
+            modifier = Modifier
+                .fillMaxSize()
+                .background(color = APPTheme.colors.background)
         ) {
             val context = LocalContext.current
             Text(
                 modifier = Modifier.padding(start = 16.dp, top = 16.dp, bottom = 8.dp),
                 text = context.getString(R.string.develop_menu_label),
-                color = Color.Black,
+                color = APPTheme.colors.primary,
                 fontSize = 28.sp,
                 fontWeight = FontWeight.Bold
             )
