15800fd16725e4f30a7fd12fc682ee8091fefced
Steve Cheng <biao.cheng@thoughtworks.com>
2022-10-10 13:08:45 +0800

    add test for FeedRepository

---
+++ app/src/test/java/com/thoughtworks/ark/ui/home/feeds/data/repository/FeedRepositoryTest.kt
@@ -0,0 +1,38 @@
+package com.thoughtworks.ark.ui.home.feeds.data.repository
+
+import com.google.common.truth.Truth.assertThat
+import com.thoughtworks.ark.core.network.entity.Result
+import com.thoughtworks.ark.ui.home.feeds.data.repository.entity.FeedListEntity
+import com.thoughtworks.ark.ui.home.feeds.data.repository.source.FeedApiDataSource
+import io.mockk.coEvery
+import io.mockk.coVerify
+import io.mockk.mockk
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.flow.flowOf
+import kotlinx.coroutines.test.runTest
+import org.junit.Before
+import org.junit.Test
+
+@OptIn(ExperimentalCoroutinesApi::class)
+class FeedRepositoryTest {
+
+    private val feedList = FeedListEntity(0, null)
+
+    private val feedApiDataSource = mockk<FeedApiDataSource>()
+    private val feedRepository = FeedRepository(feedApiDataSource)
+
+    @Before
+    fun setUp() {
+        coEvery { feedApiDataSource.getFeedList() } returns flowOf(Result.Success(feedList))
+    }
+
+    @Test
+    fun `should call data source when fetch feed list from repository`() = runTest {
+        // when
+        val result = feedRepository.getFeedList().first()
+        // then
+        coVerify(exactly = 1) { feedApiDataSource.getFeedList() }
+        assertThat((result as Result.Success).data).isEqualTo(feedList)
+    }
+}
