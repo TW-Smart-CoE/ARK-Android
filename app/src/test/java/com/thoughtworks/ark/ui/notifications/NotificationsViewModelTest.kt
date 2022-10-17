9e82ba7f6f6d9c512ee1e71f63900d2e27965087
Zhang Shuai <shuaiz@thoughtworks.com>
2022-10-17 18:38:25 +0800

    add unit test for notification view model

---
+++ app/src/test/java/com/thoughtworks/ark/ui/notifications/NotificationsViewModelTest.kt
@@ -0,0 +1,18 @@
+package com.thoughtworks.ark.ui.notifications
+
+import com.google.common.truth.Truth.assertThat
+import org.junit.Test
+
+class NotificationsViewModelTest {
+    @Test
+    fun shouldInitialNotificationsWhenCreated() {
+        // given
+        val viewModel = NotificationsViewModel()
+
+        // when
+        val item = viewModel.state
+
+        // then
+        assertThat(item.label).isEqualTo("This is notifications Fragment")
+    }
+}
