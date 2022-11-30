56e7a3d49e90445768161a6da8570afa5910afeb
weiran1999 <weiran.sun@thoughtworks.com>
2022-11-30 11:55:33 +0800

    feat: alter content

--- core/src/main/java/com/thoughtworks/ark/core/storage/FileManager.kt
+++ core/src/main/java/com/thoughtworks/ark/core/storage/FileManager.kt
@@ -1,18 +1,16 @@
-package com.thoughtworks.ark.core.utils
+package com.thoughtworks.ark.core.storage
 
 
 import android.content.Context
-import android.graphics.Bitmap
-import android.graphics.BitmapFactory
 import android.os.Environment
 import android.util.Log
 import java.io.File
 import java.io.IOException
 
 class FileManager(val context: Context) {
+
     private var filesPath: String = ""
-    private var packSavePath: String = ""
-    private var resPath: String = ""
+    private var savesPath: String = ""
 
     private val tag = "FileManager"
 
@@ -27,13 +25,10 @@ class FileManager(val context: Context) {
             File(filesPath).mkdir()
         }
 
-        packSavePath = packageFile.path + "/saves"
-        if (!File(packSavePath).exists()) {
-            File(packSavePath).mkdir()
+        savesPath = packageFile.path + "/saves"
+        if (!File(savesPath).exists()) {
+            File(savesPath).mkdir()
         }
-
-        Log.i("storage2", "filesPath : $filesPath")
-        Log.i("storage2", "packSavePath : $packSavePath")
     }
 
     fun listSDCardFiles(): List<String> {
@@ -69,8 +64,8 @@ class FileManager(val context: Context) {
 
     fun outPutFile(fileName: String, fileContent: String) {
         try {
-            val file = File(packSavePath, fileName)
-            Log.i(tag, "outPutFile : $packSavePath, name: $fileName")
+            val file = File(savesPath, fileName)
+            Log.i(tag, "outPutFile : $savesPath, name: $fileName")
             file.writeText(fileContent)
         } catch (e: IOException) {
             Log.e(tag, "outPutFile exception ${e.stackTrace}")
@@ -78,25 +73,4 @@ class FileManager(val context: Context) {
     }
 
 
-    fun loadResImage(imageFileName: String): Bitmap? {
-        val file = File(resPath, imageFileName)
-        if (!file.exists()) {
-            return null
-        }
-
-        return BitmapFactory.decodeFile(file.absolutePath)
-    }
-
-    fun loadFile(fileName: String): File? {
-        val file = File(resPath, fileName)
-        if (!file.exists()) {
-            return null
-        }
-
-        return file
-    }
-
-    fun checkFileExist(imageFileName: String): Boolean {
-        return File(resPath, imageFileName).exists()
-    }
 }
