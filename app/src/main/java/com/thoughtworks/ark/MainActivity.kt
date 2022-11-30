31e166cb7deed0dd1bcb1ead971e2423533abd1c
weiran1999 <weiran.sun@thoughtworks.com>
2022-11-30 10:15:24 +0800

    feat: remove sample content

--- app/src/main/java/com/thoughtworks/ark/MainActivity.kt
+++ app/src/main/java/com/thoughtworks/ark/MainActivity.kt
@@ -1,14 +1,12 @@
 package com.thoughtworks.ark
 
 import android.os.Bundle
-import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
-import com.thoughtworks.ark.core.utils.ExternalFileUtils
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -28,13 +26,5 @@ class MainActivity : AppCompatActivity() {
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
-        Log.i("storage1", "判断SD卡是否被挂载 " + ExternalFileUtils.isSDCardMounted.toString())
-        Log.i("storage1", "获取SD卡的根目录 " + ExternalFileUtils.sDCardBaseDir)
-        Log.i("storage1", "获取SD卡的完整空间大小，返回MB " + ExternalFileUtils.sDCardSize.toString())
-        Log.i("storage1", "获取SD卡的剩余空间大小 " + ExternalFileUtils.sDCardFreeSize.toString())
-        Log.i("storage1", "获取SD卡的可用空间大小 " + ExternalFileUtils.sDCardAvailableSize.toString())
-        Log.i("storage1", "获取文件 " + ExternalFileUtils.loadFileFromSDCard(""))
-
-
     }
 }
