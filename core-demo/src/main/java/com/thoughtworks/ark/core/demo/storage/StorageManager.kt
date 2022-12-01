31a64391898cf6664f76cc30659824a80f3ce417
weiran1999 <weiran.sun@thoughtworks.com>
2022-12-01 17:46:06 +0800

    feat: add loadResImage

--- core-demo/src/main/java/com/thoughtworks/ark/core/demo/storage/StorageManager.kt
+++ core-demo/src/main/java/com/thoughtworks/ark/core/demo/storage/StorageManager.kt
@@ -1,5 +1,7 @@
 package com.thoughtworks.ark.core.demo.storage
 
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import org.json.JSONObject
 import java.io.File
 
@@ -18,6 +20,14 @@ object StorageManager {
     fun loadJson(resPath: String, fileName: String, jsonKey: String): String? {
         val jsonContent = loadFile(resPath, fileName)?.readText()
         return jsonContent?.let { JSONObject(it).getJSONObject(jsonKey).toString() }
+    }
+
+    fun loadResImage(resPath: String, imageFileName: String): Bitmap? {
+        val file = File(resPath, imageFileName)
+        if (!file.exists()) {
+            return null
+        }
+        return BitmapFactory.decodeFile(file.absolutePath)
     }
 
 
