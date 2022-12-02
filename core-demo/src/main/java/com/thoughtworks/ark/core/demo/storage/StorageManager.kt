de3d7cdbf49a5703c8af38a98eab7a77aa5870ac
weiran1999 <weiran.sun@thoughtworks.com>
2022-12-02 09:28:14 +0800

    feat: add some sdCard method

--- core-demo/src/main/java/com/thoughtworks/ark/core/demo/storage/StorageManager.kt
+++ core-demo/src/main/java/com/thoughtworks/ark/core/demo/storage/StorageManager.kt
@@ -2,6 +2,8 @@ package com.thoughtworks.ark.core.demo.storage
 
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
+import android.os.Environment
+import android.os.StatFs
 import android.util.Log
 import org.json.JSONObject
 import java.io.File
@@ -45,7 +47,61 @@ object StorageManager {
         }
     }
 
+    fun removeFile(resPath: String, fileName: String): Boolean {
+        val file = File(resPath, fileName)
+        if (file.exists()) {
+            return try {
+                file.delete()
+                true
+            } catch (e: Exception) {
+                false
+            }
+        }
+        return false
+    }
 
+    val isSDCardMounted: Boolean
+        get() =
+            (Environment.getExternalStorageState() ==
+                    Environment.MEDIA_MOUNTED)
+
+    val sDCardBaseDir: String?
+        get() = if (isSDCardMounted) {
+            Environment.getExternalStorageDirectory().absolutePath
+        } else null
+
+    val sDCardSize: Long
+        get() {
+            if (isSDCardMounted) {
+                val statFs = StatFs(sDCardBaseDir)
+                val count = statFs.blockCountLong
+                val size = statFs.blockSizeLong
+                return count * size / 1024 / 1024
+            }
+            return 0
+        }
+
+    val sDCardFreeSize: Long
+        get() {
+            if (isSDCardMounted) {
+                val statFs = StatFs(sDCardBaseDir)
+                val count = statFs.freeBlocksLong
+                val size = statFs.blockSizeLong
+                return count * size / 1024 / 1024
+            }
+            return 0
+        }
+
+    val sDCardAvailableSize: Long
+        get() {
+            if (isSDCardMounted) {
+                val statFs = StatFs(sDCardBaseDir)
+                val count = statFs.availableBlocksLong
+                val size = statFs.blockSizeLong
+                return count * size / 1024 / 1024
+            }
+            return 0
+        }
 
 }
 
