683ab628fbc6a13b94bad5afc7304a5c96439a2c
weiran1999 <weiran.sun@thoughtworks.com>
2022-12-06 12:26:59 +0800

    feat: alter screen

--- sample/src/main/java/com/thoughtworks/ark/sample/storage/StorageScreen.kt
+++ sample/src/main/java/com/thoughtworks/ark/sample/storage/StorageScreen.kt
@@ -2,18 +2,31 @@ package com.thoughtworks.ark.sample.storage
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.ParagraphStyle
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.withStyle
+import androidx.compose.ui.unit.sp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.thoughtworks.ark.ui.component.AppFilledButton
 import com.thoughtworks.ark.ui.theme.Dimensions
 import com.thoughtworks.ark.ui.theme.Theme
 
 @Composable
-fun StorageScreen() {
+fun StorageScreen(viewModel: StorageViewModel = viewModel()) {
+
+    val state = viewModel.storageState.collectAsState().value
 
     Column(
         modifier = Modifier
@@ -23,7 +36,7 @@ fun StorageScreen() {
     ) {
         AppFilledButton(
             modifier = Modifier.fillMaxWidth(),
-            onClick = { },
+            onClick = { viewModel.updateStorageState() },
             text = {
                 Text(text = "demo 1")
             }
@@ -52,5 +65,56 @@ fun StorageScreen() {
                 Text(text = "demo 4")
             }
         )
+
+        Row(
+            modifier = Modifier.padding(
+                vertical = Dimensions.dimension128,
+                horizontal = Dimensions.dimension128
+            ),
+        ) {
+            MyText(state.fileIsFlag)
+        }
     }
+
+}
+
+@Composable
+private fun MyText(fileIsFlag: Boolean) {
+    Text(
+        buildAnnotatedString {
+            withStyle(
+                style = ParagraphStyle(
+                    lineHeight = 30.sp,
+                    textAlign = TextAlign.Center
+                )
+            ) {
+                withStyle(
+                    style = SpanStyle(
+                        color = Color.Blue,
+                        fontWeight = FontWeight.Bold
+                    )
+                ) {
+                    append("check result\n")
+                }
+                withStyle(
+                    style = SpanStyle(
+                        color = Color.Blue,
+                        fontWeight = FontWeight.Bold,
+                        fontSize = 20.sp
+                    )
+                ) {
+                    append("is\n")
+                }
+                withStyle(
+                    style = SpanStyle(
+                        color = Color.Red,
+                        fontWeight = FontWeight.Bold,
+                        fontSize = 30.sp
+                    )
+                ) {
+                    append(fileIsFlag.toString())
+                }
+            }
+        }
+    )
 }
