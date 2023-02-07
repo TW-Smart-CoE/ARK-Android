faf283c7b6a6123f33a6bba4302b9b6552e3b535
Zhang Guo <guo.zhang@thoughtworks.com>
2023-02-07 14:55:15 +0800

    feat: use standalone build logic

--- build-logic/src/main/kotlin/com/thoughtworks/ark/buildlogic/AppConfig.kt
+++
@@ -1,9 +0,0 @@
-package com.thoughtworks.ark.buildlogic
-
-object AppConfig {
-    const val APP_SCHEME_KEY = "APP_SCHEME"
-    const val APP_SCHEME_VALUE = "ark"
-
-    const val APP_HOST_KEY = "APP_HOST"
-    const val APP_HOST_VALUE = "app"
-}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,12 +1,40 @@
 @file:Suppress("DSL_SCOPE_VIOLATION", "UnstableApiUsage")
 
+import java.io.ByteArrayOutputStream
+
+fun initBuildLogic() {
+    fun execCmd(cmd: String): String {
+        val stdout = ByteArrayOutputStream()
+        exec {
+            commandLine(cmd.split(" "))
+            standardOutput = stdout
+        }
+        return stdout.toString().trim()
+    }
+
+    if (!file("BuildLogic").exists()) {
+        println("Init build logic...")
+        //clone build logic to BuildLogic dir
+        val result = execCmd("git clone -b main https://github.com/TW-Smart-CoE/BuildLogic.git BuildLogic")
+        print(result)
+        println("Build logic init success")
+    } else {
+        println("Update build logic...")
+        val result = execCmd("cd BuildLogic git pull origin/main")
+        print(result)
+        println("Update build logic success")
+    }
+}
+
+initBuildLogic()
+
 fun readConfig(name: String): String {
     return settings.extensions.extraProperties.properties[name] as String?
         ?: System.getenv(name) ?: ""
 }
 
 pluginManagement {
-    includeBuild("build-logic")
+    includeBuild("BuildLogic")
     repositories {
         gradlePluginPortal()
         google()
